269eb9ddbf6ad16a6040f80792682179dcb56b30
mk0537 <xodus2331@naver.com>
2025-04-02 09:46:12 +0900

    java 10일차

--- ex_0401/src/ex03_Map/Ex02_Map.java
+++ ex_0401/src/ex03_Map/Ex02_Map.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Ex02_Map {
 	public static void main(String[] args) {
+
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 
 		Scanner sc = new Scanner(System.in);
@@ -22,19 +23,25 @@ public class Ex02_Map {
 		// pw : 3333
 		// 로그인 성공!
 		while (true) {
-			System.out.print("id :");
-			String id = sc.next();
-
-			System.out.print("pw :");
+			System.out.print("id : ");
+			String id1 = sc.next();
+			System.out.print("pw : ");
 			int pw = sc.nextInt();
 
-			if (map.containsKey(id)) {
-				if(map.get(id)== id && map.get(pw)==pw) {
-					
-				}
+			if (!map.containsKey(id1)) {
+				System.out.println("아이디가 존재하지 않습니다.");
+				continue;
 			}
 
+			
+			if (map.get(id1) == pw) {
+				System.out.println("로그인 성공!");
+				break;
+			} else {
+				System.out.println("비밀번호 불일치");
+			}
 		}
 
 	}
+
 }
